c2860e6998fa330ec7b0646b6a68976f9d228c5e
ashmeet <ashmeetchhabra111@gmail.com>
2025-01-05 20:28:26 +0000

    troubleshooting

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
+import numpy
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
@@ -8,7 +9,8 @@ register_matplotlib_converters()
 df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col=['date'])
 
 # Clean data
-df = df.query('value > value.quantile(0.025) and value < value.quantile(1-0.025)')
+# df = df.query('value > value.quantile(0.025) and value < value.quantile(1-0.025)')
+df = df[(df['value'] > df['value'].quantile(0.025)) & (df['value'] < df['value'].quantile(1 - 0.025))]
 
 def draw_line_plot():
     # Draw line plot
@@ -25,7 +27,9 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = df.groupby(['year', 'month'])['value'].sum()
+    df['month'] = df.index.month
+    df['year'] = df.index.year
+    df_bar = df.groupby(['year', 'month'])['value'].sum().unstack()
 
     # Draw bar plot
     fig = df_bar.plot(kind='bar').get_figure()
@@ -45,10 +49,18 @@ def draw_box_plot():
     df_box.reset_index(inplace=True)
     df_box['year'] = [d.year for d in df_box.date]
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
-
     # Draw box plots (using Seaborn)
 
-
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+    ax1.set_title('Year-wise Box Plot (Trend)')
+    ax1.set_xlabel('Year')
+    ax1.set_ylabel('Page Views')
+    sns.boxplot(data=df_box, x='month', y='value', ax=ax2)
+    sns.boxplot(data=df_box, x='year', y='value', ax=ax1)
+    ax2.set_title('Month-wise Box Plot (Seasonality)')
+    ax2.set_xlabel('Month')
+    ax2.set_ylabel('Page Views')
+    plt.tight_layout()
 
 
 
